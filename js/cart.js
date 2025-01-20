0a5bfab40a04027b3627aa4350617bd5b0f4892b
AliElsese <ali.elsese104@gmail.com>
2025-01-20 13:09:45 -0800

    third commit

--- js/cart.js
+++ js/cart.js
@@ -15,28 +15,32 @@ function displayCart() {
         cartSection.innerHTML += `
             <div class="card-item d-flex justify-content-between align-items-center rounded-3 shadow-lg p-3 mb-4">
                 <img class="rounded" src=${product.image} width="90">
-                <p class="m-0 fw-bold">${product.name}</p>
-                <div class="item-qty d-flex align-items-center gap-3">
-                    <button class="fw-bold text-danger bg-transparent border-0" onClick="decreaseQuantity(${index})">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24">
-                            <path fill="currentColor" d="M19 11H5a1 1 0 0 0 0 2h14a1 1 0 0 0 0-2" />
-                        </svg>
-                    </button>
-                    <h5 class="text-grey m-0">${product.qty}</h5>
-                    <button class="fw-bold text-primary bg-transparent border-0" onClick="increaseQuantity(${index})">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24">
-                            <path fill="currentColor" d="M13 6a1 1 0 1 0-2 0v5H6a1 1 0 1 0 0 2h5v5a1 1 0 1 0 2 0v-5h5a1 1 0 1 0 0-2h-5z" />
-                        </svg>
-                    </button>
+                <div class="col-6 col-md-9 d-flex flex-column gap-3 flex-md-row justify-content-between align-items-center">
+                    <p class="m-0 fw-bold">${product.name}</p>
+                    <div class="item-qty d-flex align-items-center gap-3">
+                        <button class="fw-bold text-danger bg-transparent border-0" onClick="decreaseQuantity(${index})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M19 11H5a1 1 0 0 0 0 2h14a1 1 0 0 0 0-2" />
+                            </svg>
+                        </button>
+                        <h5 class="text-grey m-0">${product.qty}</h5>
+                        <button class="fw-bold text-primary bg-transparent border-0" onClick="increaseQuantity(${index})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M13 6a1 1 0 1 0-2 0v5H6a1 1 0 1 0 0 2h5v5a1 1 0 1 0 2 0v-5h5a1 1 0 1 0 0-2h-5z" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="col-12 col-md-3 d-flex justify-content-md-between justify-content-center gap-4 gap-md-0 align-items-center">
+                        <div class="item-price d-flex align-items-center">
+                        <h5 class="text-grey m-0">${product.price * product.qty} $</h5>
+                        </div>
+                        <button class="text-danger bg-transparent border-0" onClick="removeItem(${index})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
-                <div class="item-price d-flex align-items-center">
-                    <h5 class="text-grey m-0">${product.price * product.qty} $</h5>
-                </div>
-                <button class="text-danger bg-transparent border-0" onClick="removeItem(${index})">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z" />
-                    </svg>
-                </button>
             </div>
         `;
     });
